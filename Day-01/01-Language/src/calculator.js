11a66f0e27b28b240525768c84878f7b8b5c25b6
tkmagesh <tkmagesh77@gmail.com>
2015-06-24 12:33:06 +0530

    Closures - Seed

--- Day-01/01-Language/src/calculator.js
+++ Day-01/01-Language/src/calculator.js
@@ -1,3 +1,23 @@
 function add(x,y){
-    return x + y;
+    function parseArg(n){
+        if (Array.isArray(n)) return add.apply(this, n);
+        if (typeof n === "function") return parseArg(n());
+        return isNaN(n) ? 0 : parseInt(n,10);
+    }
+    var result = 0;
+    for(var i=0; i<arguments.length; i++)
+        result += parseArg(arguments[i]);
+    return result;
 }
+
+/*
+Function invocation patterns
+
+1. As a method of obj
+      this -> obj
+
+2. As a function
+      this -> global scope (window)
+
+
+*/
